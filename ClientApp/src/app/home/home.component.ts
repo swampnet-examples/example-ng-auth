42b41811fb1b2cebd4432b17ca323bcab68d52f0
Pj <petejw@theswamp.co.uk>
2018-05-20 16:53:21 +0100

    Force login on homepage

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+    if(!this.auth.isAuthenticated()){
+        this.auth.login();
+    }
+  }
 }
